a39b43d7a23d5f0fdf87bd2c79bf38891bf54b3c
Taylor Gilbert <taylorg353@gmail.com>
2015-06-07 23:59:36 -0700

    Initial Commit

---
+++ commuteTimer/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  commuteTimer
+//
+//  Created by Taylor H. Gilbert on 6/7/15.
+//  Copyright (c) 2015 Taylor H. Gilbert. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
